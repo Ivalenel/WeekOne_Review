d3aebe73437a1ddf66d1fc6b58ed973684d88763
Ivalene <ivalenelarrea234@gmail.com>
2024-09-30 10:49:42 -0500

    first commit

---
+++ src/main/java/com/ps/Calculator.java
@@ -0,0 +1,23 @@
+package com.ps;
+
+class Calculator {
+    public static void main(String[] args) {
+
+        int num1 = 2;
+        int num2 = 4;
+        int num3 = 1;
+
+    //Static add method
+
+
+    //Static subtract method
+
+
+    //Static multiplication
+
+
+    //Static division
+
+    //Receive two inputs (ints)
+    //Return a single number (ints, double)
+}
